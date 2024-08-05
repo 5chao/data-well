e4bb6b8c58107b5541531c3afb2f871314d32faf
李武朝 <1974103331@qq.com>
2024-08-05 10:22:05 +0800

    perf:静态资源

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import modulesList from './modules';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [...modulesList],
+  imports: [
+    ...modulesList,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'static'),
+      serveRoot: '/static',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
